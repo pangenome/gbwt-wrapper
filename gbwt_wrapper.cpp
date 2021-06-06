707dbb1c7377cc856818322d0098487fd926137d
Christos Chatzifountas <heefoo@outlook.com>
2021-06-06 15:13:48 +0300

    Add index methods as functions

--- gbwt_wrapper.cpp
+++ gbwt_wrapper.cpp
@@ -1,6 +1,7 @@
 // #include"/usr/local/include/gbwt/utils.h"
 #include"/usr/local/include/gbwt/dynamic_gbwt.h"
 #include"gbwt_wrapper.h"
+#include"/usr/local/include/gbwt/gbwt.h"
 
 void* newDynamicGBWT(void) {
 
@@ -27,10 +28,87 @@ void insertGBWTSequence (void *  dynGBWT, text_type& text) {
 
 
 
-int main (void)  {
-        printf("Hello world");
-        void* the_gbwt = newDynamicGBWT();
-        deleteDynamicGBWT(the_gbwt);
-        return 0;
+void* DynamicGBWT_to_GBWT(void* dynGBWT) {
+
+        gbwt::DynamicGBWT* tmp = (gbwt::DynamicGBWT *)tmp;
+        gbwt::GBWT item =   gbwt::GBWT(*tmp) ;
+        gbwt::GBWT * pitem = &item;
+        return (void *) pitem;
+}
+
+
+void  deleteGBWT(void * GBWT) {
+
+
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        delete  gbwt;
 
 }
+
+
+//methods
+
+size_type total_path_length (void * GBWT) {
+
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->size();
+
+}
+
+
+
+bool has_empty_index (void * GBWT) {
+
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->empty();
+}
+
+
+
+
+size_type number_of_paths(void* GBWT)
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->sequences();
+}
+
+
+size_type alphabet_size(void* GBWT)
+
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->sigma();
+}
+
+
+size_type effective_alpahbet_size(void* GBWT)
+
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->effective();
+}
+
+size_type number_of_samples(void* GBWT)
+
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->samples();
+}
+
+
+
+bool is_index_bidirectional(void* GBWT)
+
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->bidirectional();
+}
+
+std::pair<size_type, size_type>   number_of_gwt_runs   (void* GBWT)
+
+        {
+        gbwt::GBWT* gbwt = (gbwt::GBWT *) GBWT;
+        return gbwt->runs();
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include<stdio.h>
-#include"gbwt.h"
-
+#include"gbwt_wrapper.h"
 #include"/usr/local/include/gbwt/gbwt.h"
 
 int main(void){
+        void* skata = newDynamicGBWT() ;
+        deleteDynamicGBWT(skata);
         printf("Hello world");
         return 0;
 }
